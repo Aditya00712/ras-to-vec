7dccc5b2cc28478a91143d8aa3f8340f57ac29a2
Aditya00712 <aa2167906@gmail.com>
2025-05-02 18:51:32 +0530

    Comment out unused URL path for converting TIFF to GeoJSON in urls.py

--- core/urls.py
+++ core/urls.py
@@ -2,6 +2,6 @@ from django.urls import path, include
 from .views import *
 
 urlpatterns = [
-    path('convert/', convert_tif_to_geojson, name='convert_tif_to_geojson'),
+    # path('convert/', convert_tif_to_geojson, name='convert_tif_to_geojson'),
     path('convert-raster/', raster_to_vector_view, name='raster_to_vector_view'),
 ]
